715b979eac14019bceb8a2fb7a30d9b6cd772439
Sarthak Pednekar <Sarthak.pednekar@iterforge.com>
2026-02-27 10:48:34 +0530

    Actuator task pushed

---
+++ Product-Service/src/main/java/com/example/Product_Service/actuator/HealthCheck.java
@@ -0,0 +1,19 @@
+package com.example.Product_Service.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthCheck implements HealthIndicator {
+    @Override
+    public Health getHealth(boolean includeDetails) {
+        return HealthIndicator.super.getHealth(includeDetails);
+    }
+
+    @Override
+    public Health health() {
+        return Health.up()
+                .withDetail("service","Product-Service is Running").build();
+    }
+}
